c8f6802df5a1d2fc15289f8d3822c29d9422e9d1
Thomas Kint <thomaskint.pro@gmail.com>
2017-10-16 13:53:02 +0200

    Ajout de vuetify et axios

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,68 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app dark toolbar>
+    <v-toolbar class="indigo" dark fixed>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Block Chain</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-snackbar class="white" top right v-model="searchbox" :timeout="0">
+        <form method="get" v-on:submit.prevent="searchBook">
+          <v-text-field prepend-icon="search" label="Search book..." hide-details single-line autofocus v-model="query"></v-text-field>
+        </form>
+        <v-btn icon class="darken-12 grey--text" @click.native.stop="searchbox = !searchbox">
+          <v-icon>close</v-icon>
+        </v-btn>
+      </v-snackbar>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <transition name="slide-fade" mode="out-in">
+          <router-view :key="routerKey"></router-view>
+        </transition>
+      </v-container>
+    </main>
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: 'app',
-};
+  require('vuetify/dist/vuetify.min.css');
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        query: '',
+        number: 0,
+        permanent: true,
+        temporary: false,
+        mini: true,
+        more: false,
+        pages: [
+          { name: 'Home', target: 'Hello', icon: 'home', fa: false, mobile: true, external: false },
+        ],
+        searchbox: false,
+      };
+    },
+    created() {
+    },
+    watch: {
+    },
+    computed: {
+    },
+    methods: {
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .slide-fade-enter-active {
+    transition: all .1s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-fade-enter, .slide-fade-leave-to
+    /* .slide-fade-leave-active for <2.1.8 */ {
+    transform: translateY(10px);
+    opacity: 0;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+import VueAxios from 'vue-axios';
+import Axios from 'axios';
+
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Axios.defaults.baseURL = 'http://82.224.149.137/BookKeeperWebServices/';
+// Axios.defaults.headers.Authorization = 'Basic dGtpbnQ6MzYyNjZacnBwYy4';
+
+Vue.use(Vuetify);
+Vue.use(VueAxios, Axios);
 
 /* eslint-disable no-new */
 new Vue({
